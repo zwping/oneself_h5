8ca31275fd71450fab977dc0a9a7fff5be039261
zwping <1101558280@qq.com>
2020-04-01 21:52:09 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,9 @@
     <div>
       <div class="">
         <lgbg v-if="nullToken">登录窗口</lgbg>
-        <loading v-if="tokenVerifyIng">校验token</loading>
         <frameLayout v-if="tokenVerifySuc">登录成功</frameLayout>
-<!--        <rate-progress></rate-progress>-->
+        <loading v-if="tokenVerifyIng">校验token</loading>
+        <!--        <rate-progress></rate-progress>-->
       </div>
     </div>
   </div>
