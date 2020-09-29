ec6247389cf5c37e4c11441139f5cde117801548
zwping <1101558280@qq.com>
2020-09-29 14:55:53 +0800

    [empty] initial commit

--- mock_server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express') // 引入express插件
-const app = express() // 使用express插件
-app.use(express.static('../dist')) // 使用把生成的dist复制到文件夹里作为静态文件交给node管理
-app.listen(8090, () => {
-    console.log(
-        'mock server success\nrunning at:\n' + '- Local: http://localhost:8090',
-    )
-})
--- src/libs/CopyUtil.js
+++
@@ -1,8 +0,0 @@
-export function copy(text) {
-    let input = document.createElement('input')
-    input.value = text
-    document.body.appendChild(input)
-    input.select()
-    document.execCommand('Copy')
-    document.body.removeChild(input)
-}
